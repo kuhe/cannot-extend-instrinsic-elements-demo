e5886c049320915a03e2c6c9d19917477e3c9274
George Fu <gfu@grubhub.com>
2019-07-19 14:19:00 -0400

    hmm

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var preact_1 = require("preact");
+function main() {
+    return preact_1.h("my-custom-element", { attr: 5 });
+}
+exports.main = main;
+main();
---
+++ main.tsx
@@ -0,0 +1,7 @@
+import { h as createElement } from 'preact';
+
+export function main() {
+    return <my-custom-element attr={5} />
+}
+
+main();
